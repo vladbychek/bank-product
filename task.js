25640c14106b55416fd6b57c4a5aebcbabb9219f
Vlad <vladbychek3@gmail.com>
2022-09-22 02:44:49 +0300

    finished 2))

--- task.js
+++ task.js
@@ -2,20 +2,18 @@ let productOfBank = []
 
 class Product {
     constructor(name, percent, timeOfCap){
-        this.name = name,
-        this.percent = percent,
-        this.timeOfCap = timeOfCap
+        this.name = name;
+        this.percent = percent;
+        this.timeOfCap = timeOfCap;
     }
 }
 
-
-function bankValues() {
+function addProduct() {
     let newProduct = new Product (
-        document.querySelector('.bank__val1').value,
-        document.querySelector('.bank__val2').value,
-        document.querySelector('.bank__val3').value
+        document.querySelector('.bank__name').value,
+        document.querySelector('.bank__percent').value,
+        document.querySelector('.bank__cap_term').value
     )
-
     productOfBank.push(newProduct)
 
     let option = document.createElement('option')
@@ -23,48 +21,84 @@ function bankValues() {
     user__product.append(option)
 }
 
-document.querySelector('.bank__btn').addEventListener('click', bankValues)
+document.querySelector('.bank__send-to-user').addEventListener('click', addProduct)
 
-function userValues() {
-    return [
-        document.querySelector('.user__select').value,
-        document.querySelector('.user__val2').value,
-        document.querySelector('.user__val3').value
-    ];
-};
+function getUserValues() {
+    return {
+        name: document.querySelector('.user__select-name').value,
+        amount: document.querySelector('.user__amount').value,
+        period: document.querySelector('.user__period').value
+    }
+}
 
 function formula() {
-    let userValue = userValues()
+    let userValue = getUserValues()
     for(let i = 0; i < productOfBank.length; i++) {
-        let bankValue = Object.values(productOfBank[i])
-            if(userValue[0] === bankValue[0] ){
-                let result = userValue[1] * (1 + bankValue[1] / 100 / userValue[2]) ** (bankValue[2])
-                addResult(`User: ${userValue[0]}<br></br>Amount:${userValue[1]}<br></br>Result:${result}`)
-            }
+        let bankValue = productOfBank[i]
+        if(userValue.name === bankValue.name ){
+            let result = userValue.amount * (1 + bankValue.percent / 100 / userValue.period) ** (bankValue.timeOfCap)
+            addResult(`User: ${userValue.name}<br></br>Amount: ${userValue.amount}<br></br>Result: ${result}`)
+        }
     }
 }
 
 function addResult(formula) {
-    let cash = document.createElement('div');
-    cash.className = 'cash';
-    cash.innerHTML = String(formula);
-    show.append(cash);
+    let cash = document.createElement('div')
+    cash.className = 'cash'
+    cash.innerHTML = String(formula)
+    show.append(cash)
 }
-
-
 
 function clear() {
     let inputs = document.querySelectorAll('input')
-    let selects = document.querySelectorAll('select')
     for( i = 0; i < inputs.length; i++){
         inputs[i].value = ''
     }
-    for( i = 0; i < selects.length; i++){
-        selects[i].value = ''
-    }
 }
 
+document.querySelector('.user__get-result').addEventListener('click', formula)
+document.querySelector('.clear-btn').addEventListener('click', clear)
 
-
-document.querySelector('.user__btn').addEventListener('click', formula)
-document.querySelector('.user__btn').addEventListener('click', lear)
+let bank__name = document.querySelector('.bank__name');
+let bank__percent = document.querySelector('.bank__percent');
+let bank__cap_term = document.querySelector('.bank__cap_term');
+let user__amount = document.querySelector('.user__amount');
+let user__period = document.querySelector('.user__period');
+let btn__bank = document.querySelector('.bank__send-to-user');
+let btn__user = document.querySelector('.user__get-result');
+ 
+bank__name.oninput = function(){
+  if (bank__name.value != '' && bank__percent.value != '' && bank__cap_term.value != ''){
+    btn__bank.removeAttribute('disabled');
+  }else{
+    btn__bank.setAttribute('disabled', true);
+  }
+}
+bank__percent.oninput = function(){
+    if (bank__name.value != '' && bank__percent.value != '' && bank__cap_term.value != ''){
+      btn__bank.removeAttribute('disabled');
+    }else{
+      btn__bank.setAttribute('disabled', true);
+    }
+  }
+bank__cap_term.oninput = function(){
+    if (bank__name.value != '' && bank__percent.value != '' && bank__cap_term.value != ''){
+      btn__bank.removeAttribute('disabled');
+    }else{
+      btn__bank.setAttribute('disabled', true);
+    }
+  }
+user__amount.oninput = function(){
+    if (user__amount.value != '' && user__period.value != ''){
+        btn__user.removeAttribute('disabled');
+    }else{
+        btn__user.setAttribute('disabled', true);
+    }
+  }
+user__period.oninput = function(){
+    if (user__amount.value != '' && user__period.value != ''){
+        btn__user.removeAttribute('disabled');
+    }else{
+        btn__user.setAttribute('disabled', true)
+    }
+}
