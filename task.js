595ed5349914fe048a7a237b1165fa9769b4bfb5
Vlad <vladbychek3@gmail.com>
2022-09-24 16:44:15 +0300

    100% finished

--- task.js
+++ task.js
@@ -7,6 +7,7 @@ class Product {
         this.timeOfCap = timeOfCap;
     }
 }
+
 function addProduct() {
     let newProduct
     newProduct = new Product (
@@ -20,6 +21,7 @@ function addProduct() {
     user__product.append(option)
     option.innerHTML = newProduct.name
 }
+
 function getUserValues() {
     return {
         name: document.querySelector('.user__select-name').value,
@@ -27,6 +29,7 @@ function getUserValues() {
         period: document.querySelector('.user__period').value
     }
 }
+
 function formula() {
     let userValue = getUserValues()
     for(i = 0; i < productOfBank.length; i++) {
@@ -34,15 +37,17 @@ function formula() {
         if(userValue.name === bankValue.name ){
             let result = userValue.amount * (1 + bankValue.percent / 100 / userValue.period) ** (bankValue.timeOfCap)
             addResult(`User: ${userValue.name}<br></br>Amount: ${userValue.amount}<br></br>Result: ${result}`)
-        }
+        }   
     }
 }
+
 function addResult(formula) {
     let cash = document.createElement('div')
     cash.className = 'cash'
     show.append(cash)
     cash.innerHTML = String(formula)
 }
+
 function clear() {
     let inputs = document.querySelectorAll('input')
     for( i = 0; i < inputs.length; i++){
@@ -50,50 +55,56 @@ function clear() {
     }
 }
 
+function removeEl() {
+    let elementToRemove = document.querySelector('.cash')
+    elementToRemove.remove()
+}   
+
+document.querySelector('.delete-btn').addEventListener('click', removeEl)
 document.querySelector('.bank__send-to-user').addEventListener('click', addProduct)
 document.querySelector('.user__get-result').addEventListener('click', formula)
 document.querySelector('.clear-btn').addEventListener('click', clear)
 
-let bank__name = document.querySelector('.bank__name')
-let bank__percent = document.querySelector('.bank__percent')
-let bank__cap_term = document.querySelector('.bank__cap_term')
-let user__amount = document.querySelector('.user__amount')
-let user__period = document.querySelector('.user__period')
-let btn__bank = document.querySelector('.bank__send-to-user')
-let btn__user = document.querySelector('.user__get-result')
- 
-bank__name.oninput = function(){
-  if (bank__name.value != '' && bank__percent.value != '' && bank__cap_term.value != ''){
-    btn__bank.removeAttribute('disabled')
-  }else{
-    btn__bank.setAttribute('disabled', true)
+const bank__name = document.querySelector('.bank__name');
+const bank__percent = document.querySelector('.bank__percent');
+const bank__cap_term = document.querySelector('.bank__cap_term');
+const user__amount = document.querySelector('.user__amount');
+const user__period = document.querySelector('.user__period');
+const sendBtn = document.querySelector('.bank__send-to-user');
+const getResultBtn = document.querySelector('.user__get-result');
+const clearBtn = document.querySelector('.clear-btn');
+
+function check() {
+  const bank__name_value = bank__name.value;
+  const bank__percent_value = bank__percent.value;
+  const bank__cap_term_value = bank__cap_term.value;
+  const user__amount_value = user__amount.value;
+  const user__period_value = user__period.value;
+
+  if (bank__name_value && bank__percent_value && bank__cap_term_value) {
+    sendBtn.disabled = false;
+  } else {
+    sendBtn.disabled = true;
+  }
+  if (user__amount_value && user__period_value) {
+    getResultBtn.disabled = false;
+  } else {
+    getResultBtn.disabled = true;
   }
 }
-bank__percent.oninput = function(){
-    if (bank__name.value != '' && bank__percent.value != '' && bank__cap_term.value != ''){
-      btn__bank.removeAttribute('disabled')
-    }else{
-      btn__bank.setAttribute('disabled', true)
-    }
-}
-bank__cap_term.oninput = function(){
-    if (bank__name.value != '' && bank__percent.value != '' && bank__cap_term.value != ''){
-      btn__bank.removeAttribute('disabled')
-    }else{
-      btn__bank.setAttribute('disabled', true)
-    }
-}
-user__amount.oninput = function(){
-    if (user__amount.value != '' && user__period.value != ''){
-        btn__user.removeAttribute('disabled')
-    }else{
-        btn__user.setAttribute('disabled', true)
-    }
-}
-user__period.oninput = function(){
-    if (user__amount.value != '' && user__period.value != ''){
-        btn__user.removeAttribute('disabled')
-    }else{
-        btn__user.setAttribute('disabled', true)
-    }
-}
+
+bank__name.addEventListener('input', check);
+bank__percent.addEventListener('input', check);
+bank__cap_term.addEventListener('input', check);
+user__amount.addEventListener('input', check);
+user__period.addEventListener('input', check);
+
+clearBtn.addEventListener('click', () => {
+    bank__name.value = '';
+    bank__percent.value = '';
+    bank__cap_term.value = '';
+    user__amount.value = '';
+    user__period.value = '';
+  
+  check();
+})
